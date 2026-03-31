d0d3e2a29e0eb1ff184ad85dccafffdbe68bef7e
Nuno Silva <166436702+nuno2msilva@users.noreply.github.com>
2026-03-31 21:50:58 +0000

    fix: remove optimizeCss option from experimental settings in next.config.ts

--- sushi-dash/next.config.ts
+++ sushi-dash/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     inlineCss: true,
-    optimizeCss: true,
     browsersListForSwc: true,
     optimizePackageImports: [
       "lucide-react",
